5508efce9b8ef118da2ce613384664b18e496484
teeebor@gmail.com <teeebor@gmail.com>
2016-11-28 18:38:30 +0100

    Preparation Gameobject tree structure

--- include/common/tree.h
+++ include/common/tree.h
@@ -5,8 +5,22 @@
 
 namespace reboot{
     template <class T>
-    class REBOOT_API Tree{
-        unsigned ID;
-
+    struct REBOOT_API Tree{
+        T item;
+        Tree* parent;
+        std::vector<Tree<T>*> children;
+        void addChild(T child){
+            Tree<T> *t = new Tree();
+            t->item=child;
+            t->parent=this;
+        }
+        T getItem(unsigned index){
+            return children.size()<index?children[index]->item: nullptr;
+        }
+        void removeItem(unsigned index){
+            if(children.size()<index){
+                children.erase(children.begin()+index);
+            }
+        }
     };
 }
--- include/engine/entity/gameObject.h
+++ include/engine/entity/gameObject.h
@@ -1,38 +1,64 @@
 #pragma once
 
 #include <vector>
+#include <common/tree.h>
 
 #include "../../preprocessor.h"
 
-namespace reboot
-{
-	class Component;
-	class Transform;
-	class Renderer;
+namespace reboot {
+    class Component;
 
-	class REBOOT_API GameObject
-	{
-	public:
-		const char* m_Name;
-		const Transform *transform;
-		const Renderer *renderer;
-	
-	private:
-		bool m_IsActive;
-		std::vector<Component*> components;
-		int layer;
+    class Transform;
+
+    class Renderer;
+
+    class REBOOT_API GameObject {
+    public:
+        const char *m_Name;
+        const Transform *transform;
+        const Renderer *renderer;
+
+    private:
+        Tree<GameObject*> *node;
+        bool m_IsActive;
+        std::vector<Component *> components;
+        int layer;
         unsigned int tag;
 
-	public:
-		GameObject(const char* name);
-		GameObject():GameObject("GameObject"){}
-		~GameObject();
-		template <typename T>
-		T* getComponent();
-		Component* getComponentAt(unsigned int index);
-		void addComponent(Component *component);
-		bool isActive() { return m_IsActive; }
+    public:
+        GameObject(const char *name);
+
+        GameObject() : GameObject("GameObject") {}
+
+        ~GameObject();
+
+        template<typename T>
+        T *getComponent();
+
+        GameObject *getChildAt(unsigned ID) {
+            return node->getItem(ID);
+        }
+
+        void addChild(GameObject *g) {
+            node->addChild(g);
+            g->node = node->children[node->children.size()-1];
+        }
+
+        GameObject *parent() {
+            return node->parent->item;
+        }
+
+        void removeChildAt(unsigned ID) {
+            node->removeItem(ID);
+        }
+
+        Component *getComponentAt(unsigned int index);
+
+        void addComponent(Component *component);
+
+        bool isActive() { return m_IsActive; }
+
 
         void Update();
-	};
+    };
 }
